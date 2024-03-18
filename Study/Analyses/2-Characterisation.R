899ab3ec5ae5455c4be9a876d6cad2219d2b3693
AinaSanchez <aina.sanchez.parada@estudiantat.upc.edu>
2024-03-18 09:40:33 +0000

    Update

--- Study/Analyses/2-Characterisation.R
+++ Study/Analyses/2-Characterisation.R
@@ -7,20 +7,7 @@ library(PatientProfiles)
 library(log4r)
 library(visOmopResults)
 
-# Details
-log_file        <- here(resultsFolder, "log_CharacterisationData.txt")
-logger          <- create.logger(logfile = log_file, level = "INFO")
-
-# Prepare cohorts
-cdm$vac_cohort <- cdm$vac_cohort |> 
-  mutate(cohort_start_date = as.Date(date_15years)) |>
-  select(! date_15years) |>
-  compute(name = "vac_cohort", temporary = FALSE)
-
-cdm$unvac_cohort <- cdm$unvac_cohort |>
-  mutate(cohort_start_date = as.Date(date_15years)) |>
-  select(! date_15years) |>
-  compute(name = "unvac_cohort", temporary = FALSE)
+info(logger, "CHARACTERISATION")
 
 # TABLE 1 CHARACTERISATION
 info(logger, "TABLE ONE SUMMARY")
@@ -62,57 +49,114 @@ table_one_summary <- cdm$hpv_cohorts |>
     #minCellCount = 5
   )
 
-write.csv(table_one_summary,
+write.csv(table_one_summary |> suppress(minCellCount = 5),
           paste0(resultsFolder,"/tableOne_summary_",cdmSchema,".csv"), row.names = FALSE)
 
 info(logger, "Tidy table one")
 tableOne_tidy <- table_one_summary |> 
-  as_tibble() |>
+  #as_tibble() |>
   splitAll() |>
+  mutate(across(c(age_group, window), ~replace(., is.na(.), "overall"))) |>
   pivot_wider(names_from = "cohort_name", values_from = "estimate_value") |>
-  select(! c(cdm_name, result_type, package_name, package_version))
+  select(! c(cdm_name, result_type, package_name, package_version)) |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~replace(., is.na(.), 0)))
 
-write.csv(tableOne_tidy, paste0(resultsFolder,"/tableOne_tidy_",cdmSchema,".csv"), row.names = FALSE)
+tableOne_tidy_def <- tableOne_tidy |>
+  filter(estimate_name == "count") |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.numeric(.x))) |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~replace(., . < 5, NA))) |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.character(.x))) |>
+  union_all(tableOne_tidy |>
+              filter(estimate_name != "count"))
+
+write.csv(tableOne_tidy_def, paste0(resultsFolder,"/tableOne_tidy_",cdmSchema,".csv"), row.names = FALSE)
+
 # Large Scale Characterisations
 # We want to make characterisation at index date (= vac date = cohort_start_date)
 info(logger, "Compute SMD for TableOne")
 tableOne_red <- tableOne_tidy |> 
   filter(estimate_name == "percentage") |>
-  mutate_all(~replace(., is.na(.), 0)) |>
   mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.numeric(.x)/100))
+
+tableOne_counts <- tableOne_tidy |>
+  filter(estimate_name == "count") |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.numeric(.x))) |>
+  select(age_group, variable_name, window, table, vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort) |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ replace(., . < 5, NA))) |>
+  mutate("counts (UN, VAC, UM, VM)" = paste0(unvac_cohort, ", ", vac_cohort, ", ", total_unvac_matched_cohort, ", ", total_vac_matched_cohort)) |>
+  select(! c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort))
 
 tableOne_smd <- tableOne_red |>
   mutate(original_smd = abs((vac_cohort - unvac_cohort)/sqrt((vac_cohort*(1-vac_cohort) + unvac_cohort*(1-unvac_cohort))/2))) |>
   mutate(matched_smd = abs((total_vac_matched_cohort - total_unvac_matched_cohort)/sqrt((total_vac_matched_cohort*(1-total_vac_matched_cohort) + total_unvac_matched_cohort*(1-total_unvac_matched_cohort))/2))) |>
-  mutate_all(~replace(., is.na(.), 0))
+  mutate(across(c(original_smd,matched_smd), ~replace(., is.na(.), 0))) |>
+  left_join(tableOne_counts, by = c("variable_name","age_group","window","table")) |>
+  select(! c(estimate_name, estimate_type))
+
 
 write.csv(tableOne_smd, paste0(resultsFolder,"/tableOne_SMD_",cdmSchema,".csv"), row.names = FALSE)
+
+to_help <- tableOne_tidy |>
+  filter(estimate_name == "count") |>
+  select(age_group, variable_name, window, table, vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort) |>
+  left_join(tableOne_smd |> select(age_group, variable_name, window, table, original_smd, matched_smd, "counts (UN, VAC, UM, VM)"), 
+            by = c("variable_name","age_group","window","table"))
+
 # Vaccinated cohort
 info(logger, "LARGE CHARACTERISATION SUMMARY")
-slc <- summariseLargeScaleCharacteristics(cdm$hpv_cohorts, window = list(c(-Inf, -366), c(-365, -31), c(-31, -1)), eventInWindow = c("condition_occurrence","drug_exposure"))
+slc <- summariseLargeScaleCharacteristics(cdm$hpv_cohorts, 
+                                          window = list(c(-Inf, -366), c(-365, -31), c(-31, -1)), 
+                                          eventInWindow = c("condition_occurrence","drug_exposure"),
+                                          minimumFrequency = 0
+)
 
-write.csv(slc,
+write.csv(slc |> suppress(minCellCount = 5),
           paste0(resultsFolder,"/largeScale_summary_",cdmSchema,".csv"), row.names = FALSE)
 
+# ADDD AGE GROUP
 info(logger, "Tidy Large Scale Characteristics")
 slc_tidy <- slc |>
-  as_tibble() |>
+  # as_tibble() |>
   splitAll() |>
   pivot_wider(names_from = "cohort_name", values_from = "estimate_value") |>
-  select(! c(cdm_name, result_type, package_name, package_version))
+  select(! c(cdm_name, result_type, package_name, package_version, type, analysis)) |> 
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~replace(., is.na(.), 0)))
 
-write.csv(slc_tidy, paste0(resultsFolder,"/largeScale_tidy_",cdmSchema,".csv"), row.names = FALSE)
+slc_tidy_def <- slc_tidy |>
+  filter(estimate_name == "count") |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.numeric(.x))) |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~replace(., . < 5, NA))) |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.character(.x))) |>
+  union_all(slc_tidy |>
+              filter(estimate_name != "count")) 
+
+write.csv(slc_tidy_def, 
+          paste0(resultsFolder,"/largeScale_tidy_",cdmSchema,".csv"), row.names = FALSE)
 
 info(logger, "Compute SMD for LSC")
 slc_red <- slc_tidy |> 
   filter(estimate_name == "percentage") |>
-  mutate_all(~replace(., is.na(.), 0)) |>
   mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.numeric(.x)/100))
+
+slc_counts <- slc_tidy |>
+  filter(estimate_name == "count") |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~ as.numeric(.x))) |>
+  select(variable_name, variable_level, table_name, concept, vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort) |>
+  mutate(across(c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort), ~replace(., . < 5, NA))) |>
+  mutate("counts (UN, VAC, UM, VM)" = paste0(unvac_cohort, ", ", vac_cohort, ", ", total_unvac_matched_cohort, ", ", total_vac_matched_cohort)) |>
+  select(! c(vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort))
 
 slc_smd <- slc_red |>
   mutate(original_smd = abs((vac_cohort - unvac_cohort)/sqrt((vac_cohort*(1-vac_cohort) + unvac_cohort*(1-unvac_cohort))/2))) |>
   mutate(matched_smd = abs((total_vac_matched_cohort - total_unvac_matched_cohort)/sqrt((total_vac_matched_cohort*(1-total_vac_matched_cohort) + total_unvac_matched_cohort*(1-total_unvac_matched_cohort))/2))) |>
-  mutate_all(~replace(., is.na(.), 0))
+  mutate(across(c(matched_smd, original_smd), ~replace(., is.na(.), 0))) |>
+  left_join(slc_counts, by = c("variable_name","variable_level", "table_name","concept")) |>
+  select(! c(estimate_name, estimate_type))
 
 write.csv(slc_smd, paste0(resultsFolder,"/largeScale_SMD_",cdmSchema,".csv"), row.names = FALSE)
 
+slc_help <- slc_tidy |>
+  filter(estimate_name == "count") |>
+  select(variable_name, variable_level, table_name, concept, vac_cohort, unvac_cohort, total_unvac_matched_cohort, total_vac_matched_cohort) |>
+  left_join(slc_smd |> select(variable_name, variable_level, table_name, concept, original_smd, matched_smd, "counts (UN, VAC, UM, VM)"), 
+            by = c("variable_name","variable_level", "table_name","concept"))
