96849bc081682b2c99f31cf9da9717bc58985358
AinaSanchez <aina.sanchez.parada@estudiantat.upc.edu>
2024-04-05 09:51:38 +0100

    Cleaning subpopulation definition

--- Study/Cohorts/InstantiateCohorts.R
+++ Study/Cohorts/InstantiateCohorts.R
@@ -17,8 +17,8 @@ library(RPostgres)
 if (instantiateCohorts) {
   info(logger, "INSTANTANIATE COHORTS")
   # Load vaccination records
-  #cohorts <- readCohortSet(path = here("Cohorts", "HIV_firstVac"))
-  #cdm <- generateCohortSet(cdm = cdm, cohortSet = cohorts, name = c("allvac_cohort"))
+  cohorts <- readCohortSet(path = here("Cohorts", "HIV_firstVac"))
+  cdm <- generateCohortSet(cdm = cdm, cohortSet = cohorts, name = c("allvac_cohort"))
   
   
   cdm$condition_cohort <- cdm$observation_period %>%
@@ -30,7 +30,7 @@ if (instantiateCohorts) {
     dplyr::select(!c(observation_period_id, period_type_concept_id)) %>% # eliminate unnecessary columns
     addSex() %>%
     addDateOfBirth(cdm$observation_period) %>%
-    #addAge() %>%
+    addAge() %>%
     filter(year(date_of_birth) >= 1995) %>% #, date_of_birth <= as.POSIXct("2023-12-31") - years(15)) %>%
     compute(name = "condition_cohort", temporary = FALSE) %>%
     recordCohortAttrition("Restrict to subjects born in 1995 or after") %>%
@@ -38,20 +38,20 @@ if (instantiateCohorts) {
     compute(name = "condition_cohort", temporary = FALSE) %>%
     recordCohortAttrition("Restrict to females") %>%  # Record the changes made
     mutate(date_9years = as.Date(date_of_birth + years(9))) %>%
-    filter(cohort_start_date <= date_9years) %>% #, cohort_end_date >= date_9years) %>%
-    #filter(age <= 9) %>% #, cohort_end_date >= date_9years) %>%
+    #filter(cohort_start_date <= date_9years) %>% #, cohort_end_date >= date_9years) %>%
+    filter(age <= 9) %>% #, cohort_end_date >= date_9years) %>%
     compute(name = "condition_cohort", temporary = FALSE) %>%
     recordCohortAttrition("Restrict to subjects in observation since 9yo") %>%
+    # addPriorObservation(indexDate = "date_9years") %>%
+    # filter(prior_observation >= 365) %>%
+    # compute(name = "condition_cohort", temporary = FALSE) %>%
+    # recordCohortAttrition("Restrict to subjects with prior observation >= 365 at 9yo") %>%
     mutate(date_15years = as.Date(date_of_birth + years(15))) %>%
     filter(cohort_end_date >= date_15years) %>%
     compute(name = "condition_cohort", temporary = FALSE) %>%
     recordCohortAttrition("Restrict to subjects turning 15 in observation")
   
-  #addPriorObservation(indexDate = "date_9years", priorObservationName = "prior_observation_9y") %>%
-  #filter(prior_observation_9y >= 365) %>%
-  #compute(name = "condition_cohort", temporary = FALSE) %>%
-  #recordCohortAttrition("Select subjects with at least 365 days of prior observation") 
-  
+
   # Intersect the conditioned cohort to the cohort containing all vaccinated people  
   cdm$vac_status_cohort <- cdm$condition_cohort %>%
     addCohortIntersectFlag(targetCohortTable = "allvac_cohort", window = c(-Inf, 0), indexDate = "date_15years", nameStyle = "vac_status") %>% #Intersection conditioned to vaccination before 15th birthday
